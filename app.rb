5058c5543ae35f2f6c57b63fbbf0de70fc95bc63
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 18:42:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do 
+
+    end 
+
+    post '/pirates' do 
+      erb :show
+    end 
+
     # code other routes/actions here
 
   end
