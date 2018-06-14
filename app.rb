dc209a7b8e94db8442f324b1009d89aff6f37339
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 19:51:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-
+      binding.pry
       params[:pirate][:ships].each do |args|
         Ship.new(args)
       end
